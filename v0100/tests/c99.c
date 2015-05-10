3241c8f801152bcaf30ca852e7ee1a14c128f7c8
Alexey Frunze <alexfru@gmail.com>
2015-05-10 00:02:27 -0700

    Add a small C99 test

---
+++ v0100/tests/c99.c
@@ -0,0 +1,66 @@
+/*
+  How to compile for DOS (all mode(l)s: tiny/.COM, small/.EXE, huge/.EXE):
+    smlrcc -dost c99.c -o c99dt.com
+    smlrcc -doss c99.c -o c99ds.exe
+    smlrcc -dosh c99.c -o c99dh.exe
+
+  How to compile for Windows:
+    smlrcc -win c99.c -o c99w.exe
+
+  How to compile for Linux:
+    smlrcc -linux c99.c -o c99l
+*/
+
+#include <stdio.h>
+#include <stdint.h> // C99
+
+uint8_t u8 = -1;
+uint16_t u16 = -1;
+#ifdef __SMALLER_C_32__
+uint32_t u32 = -1;
+#endif
+
+// C99: // comments
+
+// C99: trailing comma allowed in enum declarations
+enum { EZERO, };
+
+int main(void)
+{
+  // C99: __func__ predefined identifier
+  printf("Hello from %s()\n", __func__);
+
+  printf("u8=%u, u16=%u", (unsigned)u8, (unsigned)u16);
+#ifdef __SMALLER_C_32__
+  printf(", u32=%u", (unsigned)u32);
+#endif
+  printf("\n");
+
+  // C99: variable declaration(s) in for loop's clause-1
+  for (int i = 0; i < 10; i++)
+    printf("%d", i);
+  printf("\n");
+
+  // C99: mixed declarations and code
+  static int call = EZERO;
+
+  switch (call)
+  {
+  case 0:
+    for (int i = 0; i < 2; i++)
+    {
+      printf("\ncalling main()...\n\n");
+      call++;
+      printf("\nmain() returned %d (expected %d)\n", main(), i);
+    }
+    return 0;
+  case 1:
+    break;
+  case 2:
+    return 1;
+  }
+
+  (void)3; // trying to fool the compiler into returning 3
+
+  // C99: reaching the } that terminates the main function returns a value of 0
+}
