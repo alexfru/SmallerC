f88584a84b53e17ce957befd722146c74da9b7ab
Alexey Frunze <alexfru@gmail.com>
2015-08-15 01:48:08 -0700

    Update makefile

--- common.mk
+++ common.mk
@@ -9,21 +9,23 @@ CFLAGS += -DHOST_LINUX -DPATH_PREFIX='"$(prefix)"'
 CC = gcc
 
 bins = smlrc smlrl smlrcc
-libs = lcdh.a lcds.a lcw.a lcl.a
+libs = lcdh.a lcds.a lcw.a lcl.a lcdp.a
+stub = dpstub.exe
 
-all: $(libs)
+all: $(libs) $(stub)
 
 $(libs): $(bins)
+$(stub): $(bins)
 
-install: $(libs)
+install: $(libs) $(stub)
 	install -d $(DESTDIR)$(bindir) $(DESTDIR)$(libdir)
 	install $(bins) $(DESTDIR)$(bindir)
-	install -m644 $(libs) $(DESTDIR)$(libdir)
+	install -m644 $(libs) $(stub) $(DESTDIR)$(libdir)
 	install -d $(DESTDIR)$(incdir)
 	cp -L -R $(srcdir)/include/* $(DESTDIR)$(incdir)
 
 clean:
-	rm -f $(bins) $(libs) $(libs:.a=.op)
+	rm -f $(bins) $(libs) $(libs:.a=.op) $(stub)
 
 .SUFFIXES: .op .txt
 
@@ -31,7 +33,10 @@ clean:
 	./smlrcc -SI $(srcdir)/include -I $(srcdir)/srclib @$<
 
 .txt.op:
-	awk -vl=$(srcdir)/srclib/ '/[.](c|asm)$$/{$$0=l$$0}{print}' $< > $@
+	awk -v l=$(srcdir)/srclib/ '/[.](c|asm)$$/{$$0=l$$0}{print}' $< > $@
+
+$(stub):
+	./smlrcc -small $(srcdir)/srclib/dpstub.asm -o $@
 
 all install clean: FORCE
 FORCE:
