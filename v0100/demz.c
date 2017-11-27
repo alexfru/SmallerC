4c362e04e6d25a37de962ced35cad13d92b1cbbb
Alexey Frunze <alexfru@gmail.com>
2017-11-26 17:35:00 -0800

    AV workaround
    Too many antiviruses mistakenly detect malware in the
    Windows executables of the compiler.
    
    This is an attempt to work around the problem and at
    the very least make the executables downloadable.
    
    The .dat files are the .exe files with their leading
    two-character "MZ" sequence removed.
    
    The .exe files can be easily restored from the .dat
    files with the DOS/Windows 'copy' command, e.g.:
    
    copy /b m.dat + z.dat + smlrcc.dat smlrcc.exe

---
+++ v0100/demz.c
@@ -0,0 +1,27 @@
+// Strips the leading "MZ" sequence off an .EXE.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+  FILE *fin = NULL, *fout = NULL;
+  int c1, c2;
+  if (argc != 3 ||
+      (fin = fopen(argv[1], "rb")) == NULL ||
+      (fout = fopen(argv[2], "wb")) == NULL)
+    return EXIT_FAILURE;
+  c1 = fgetc(fin);
+  c2 = fgetc(fin);
+  if (c1 != 'M' || c2 != 'Z')
+  {
+    printf("%s is not an MZ .EXE!\n", argv[1]);
+    fclose(fout);
+    remove(argv[2]);
+    return EXIT_FAILURE;
+  }
+  while ((c1 = fgetc(fin)) != EOF)
+    fputc(c1, fout);
+  fclose(fout);
+  fclose(fin);
+  return 0;
+}
